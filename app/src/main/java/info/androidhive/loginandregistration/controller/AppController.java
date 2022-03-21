41b6d7cb9ae3efd195722b5781a507c21e44f69b
kifixo <danielhuici@hotmail.com>
2022-03-22 00:37:16 +0100

    Bringup

---
+++ app/src/main/java/info/androidhive/loginandregistration/controller/AppController.java
@@ -0,0 +1,46 @@
+package info.androidhive.loginandregistration.controller;
+
+import android.app.Application;
+import android.text.TextUtils;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+/**
+ * Gestiona la comunicacion con el servidor.
+ * @author Martín Gascón
+ * @author Eduardo Ruiz
+ * @author Daniel Huici
+ * @version 1.0
+ */
+public class AppController extends Application {
+
+    public static final String TAG = AppController.class.getSimpleName();
+
+    private RequestQueue mRequestQueue;
+
+    private static AppController mInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+    }
+
+    public static synchronized AppController getInstance() {
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req, String tag) {
+        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+        getRequestQueue().add(req);
+    }
+}
---
+++ app/src/main/java/info/androidhive/loginandregistration/controller/Tupla.java
@@ -0,0 +1,22 @@
+package info.androidhive.loginandregistration.controller;
+
+/**
+ * Tupla genérica de dos objetos
+ * @author Martín Gascón
+ * @author Eduardo Ruiz
+ * @author Daniel Huici
+ * @version 1.0
+ */
+public class Tupla<A, B> {
+    public final A a;
+    public final B b;
+
+    /**
+     *  Construye una tupla
+     *
+     */
+    public Tupla(A a, B b) {
+        this.a = a;
+        this.b = b;
+    }
+}
